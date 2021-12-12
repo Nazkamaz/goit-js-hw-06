bfece5245dc49ea71c8867d9a59b9c38ed6ac6bd
Nazariy Bakush <nazkamaz@gmail.com>
2021-12-12 12:01:53 +0200

    hw6.3

--- js/task-08.js
+++ js/task-08.js
@@ -6,11 +6,17 @@ event.preventDefault();
 const emailEl = event.currentTarget.elements.email;
 const passwordEl = event.currentTarget.elements.password;
 
-if(emailEl.value == '' || passwordEl.value == ''){
+if(!emailEl.value || !passwordEl.value){
   return alert('все поля должны быть заполнены.');  
   
 };
   const formData = new FormData(event.currentTarget);
+
+formData.forEach((value, name) => {
+  console.log('onFormSubmit -> name', name);
+  console.log('onFormSubmit -> value', value);
+})
+
     console.log(formData);
     formEl.reset();
   
